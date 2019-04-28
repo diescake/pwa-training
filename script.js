32ca5c0e35bfce83f457764eca2fa6e0762d9bb5
diescake <daisuke.kondo@access-company.com>
2019-04-29 03:37:08 +0900

    Implement ServiceWorker codes

---
+++ script.js
@@ -0,0 +1,13 @@
+if (!navigator.serviceWorker) {
+  return
+}
+
+window.addEventListener('load', () => {
+  navigator.serviceWorker.register('sw.js')
+  .then(sw => {
+    console.log(`ServiceWorker scope: ${sw.scope}`)
+  })
+  .catch(err => {
+    console.error(`ServiceWorker failed: ${err}`)
+  })
+})
