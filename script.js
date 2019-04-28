b558d2c0040d9436a79ccfcad4c5ae0353af3d24
diescake <daisuke.kondo@access-company.com>
2019-04-29 03:44:35 +0900

    Fix script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-if (!navigator.serviceWorker) {
-  return
-}
+;(() => {
+  if (!navigator.serviceWorker) {
+    return
+  }
 
-window.addEventListener('load', () => {
-  navigator.serviceWorker.register('sw.js')
-  .then(sw => {
-    console.log(`ServiceWorker scope: ${sw.scope}`)
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('sw.js')
+      .then(sw => {
+        console.log(`ServiceWorker scope: ${sw.scope}`)
+      })
+      .catch(err => {
+        console.error(`ServiceWorker failed: ${err}`)
+      })
   })
-  .catch(err => {
-    console.error(`ServiceWorker failed: ${err}`)
-  })
-})
+})()
